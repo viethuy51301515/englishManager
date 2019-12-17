0197708b2e3183a6aa2964c54474bdea136501ed
viethuy <viethuy51301515@gmail.com>
2019-12-17 07:07:37 +0700

    sang'

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,12 +1,32 @@
 import React from 'react';
+import {Icon,Dropdown} from 'antd';
 class Header extends React.Component{
     constructor(props){
         super(props);
     }
+
     render(){
+        const languageMenu = {
+        
+        }
         return(
             <div>
-
+                <div className='user-title'>
+                    <h5 className='item-tilte'>
+                        Huy Phan
+                    </h5>
+                    <span className='role'>
+                        admin
+                    </span>
+                </div>
+                <div>
+                    <img src="./user.png" alt=""/>
+                </div>
+                <div className='language'>
+                    <Dropdown>
+                        <Icon type="global" />
+                    </Dropdown>
+                </div>
             </div>
         )
     }
